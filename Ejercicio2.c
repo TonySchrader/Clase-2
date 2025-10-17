d25eaed2d29e50283a947725b1beeba1bfb5fe81
TonySchrader <schraderantonio@gmail.com>
2025-10-17 13:41:35 -0600

    Update Ejercicio2.c with input and math functions
    Added math library and fixed input handling.

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -2,6 +2,14 @@
 clase 2 ejercicio 2*/
 
 #include <stdio.h>
+#include <math.h>
+
 void main () 
 {
-  
+  int num1, num2, res;
+printf("Escribe el primer numero: \n");
+scan("%d", num1);
+printf("Escibe el segundo numero: \n");
+scan("%d", num2);
+
+res
