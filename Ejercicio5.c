57f00741f6eac4a0590b1319b277127ebdcec040
TonySchrader <schraderantonio@gmail.com>
2025-10-23 19:01:26 -0600

    Implement ticket pricing based on age

---
+++ Ejercicio5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    const float precio = 7.0;
+    int edad;
+    float total;
+
+    printf("Ingrese su edad: \n");
+    scanf("%d", &edad);
+
+    if (edad < 5)
+    {
+        total = precio * 0.40; // 60% de descuento
+    }
+    else if (edad > 60)
+    {
+        total = precio * 0.45; // 55% de descuento
+    }
+    else
+    {
+        total = precio;
+    }
+
+    printf("El precio de su entrada es: %.2f euros\n", total);
+}
